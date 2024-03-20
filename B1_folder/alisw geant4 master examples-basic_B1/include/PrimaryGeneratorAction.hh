727da700a90c3a4538778130e6359e024b84c8b5
abbyxh <157227938+abbyxh@users.noreply.github.com>
2024-03-20 14:02:10 +0000

    Update PrimaryGeneratorAction.hh

--- B1_folder/alisw geant4 master examples-basic_B1/include/PrimaryGeneratorAction.hh
+++ B1_folder/alisw geant4 master examples-basic_B1/include/PrimaryGeneratorAction.hh
@@ -24,8 +24,8 @@
 // ********************************************************************
 //
 //
-/// \file B1/include/PrimaryGeneratorAction.hh
-/// \brief Definition of the B1::PrimaryGeneratorAction class
+/// \file B1PrimaryGeneratorAction.hh
+/// \brief Definition of the B1PrimaryGeneratorAction class
 
 #ifndef B1PrimaryGeneratorAction_h
 #define B1PrimaryGeneratorAction_h 1
@@ -40,30 +40,25 @@ class G4Box;
 
 /// The primary generator action class with particle gun.
 ///
-/// The default kinematic is a 6 MeV gamma, randomly distribued
+/// The default kinematic is a 6 MeV gamma, randomly distribued 
 /// in front of the phantom across 80% of the (X,Y) phantom size.
 
-namespace B1
-{
-
-class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
+class B1PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
   public:
-    PrimaryGeneratorAction();
-    ~PrimaryGeneratorAction() override;
+    B1PrimaryGeneratorAction();    
+    virtual ~B1PrimaryGeneratorAction();
 
     // method from the base class
-    void GeneratePrimaries(G4Event*) override;
-
+    virtual void GeneratePrimaries(G4Event*);         
+  
     // method to access particle gun
     const G4ParticleGun* GetParticleGun() const { return fParticleGun; }
-
+  
   private:
-    G4ParticleGun* fParticleGun = nullptr; // pointer a to G4 gun class
-    G4Box* fEnvelopeBox = nullptr;
+    G4ParticleGun*  fParticleGun; // pointer a to G4 gun class
+    G4Box* fEnvelopeBox;
 };
-
-}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
