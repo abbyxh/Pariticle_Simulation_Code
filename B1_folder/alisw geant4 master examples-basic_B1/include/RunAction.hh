a8ece122566f58c80c83be92685b48533fd3bdc9
abbyxh <157227938+abbyxh@users.noreply.github.com>
2024-03-20 14:02:47 +0000

    Update RunAction.hh

--- B1_folder/alisw geant4 master examples-basic_B1/include/RunAction.hh
+++ B1_folder/alisw geant4 master examples-basic_B1/include/RunAction.hh
@@ -24,8 +24,8 @@
 // ********************************************************************
 //
 //
-/// \file B1/include/RunAction.hh
-/// \brief Definition of the B1::RunAction class
+/// \file B1RunAction.hh
+/// \brief Definition of the B1RunAction class
 
 #ifndef B1RunAction_h
 #define B1RunAction_h 1
@@ -38,30 +38,26 @@ class G4Run;
 
 /// Run action class
 ///
-/// In EndOfRunAction(), it calculates the dose in the selected volume
+/// In EndOfRunAction(), it calculates the dose in the selected volume 
 /// from the energy deposit accumulated via stepping and event actions.
 /// The computed dose is then printed on the screen.
 
-namespace B1
-{
-
-class RunAction : public G4UserRunAction
+class B1RunAction : public G4UserRunAction
 {
   public:
-    RunAction();
-    ~RunAction() override = default;
+    B1RunAction();
+    virtual ~B1RunAction();
 
-    void BeginOfRunAction(const G4Run*) override;
-    void   EndOfRunAction(const G4Run*) override;
+    // virtual G4Run* GenerateRun();
+    virtual void BeginOfRunAction(const G4Run*);
+    virtual void   EndOfRunAction(const G4Run*);
 
-    void AddEdep (G4double edep);
+    void AddEdep (G4double edep); 
 
   private:
-    G4Accumulable<G4double> fEdep = 0.;
-    G4Accumulable<G4double> fEdep2 = 0.;
+    G4Accumulable<G4double> fEdep;
+    G4Accumulable<G4double> fEdep2;
 };
-
-}
 
 #endif
 
