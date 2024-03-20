4c5370a322ebd628d70edbee7794f72f6cec5bdc
abbyxh <157227938+abbyxh@users.noreply.github.com>
2024-03-20 14:01:04 +0000

    Update EventAction.hh

--- B1_folder/alisw geant4 master examples-basic_B1/include/EventAction.hh
+++ B1_folder/alisw geant4 master examples-basic_B1/include/EventAction.hh
@@ -24,40 +24,41 @@
 // ********************************************************************
 //
 //
-/// \file B1/include/EventAction.hh
-/// \brief Definition of the B1::EventAction class
+/// \file B1PrimaryGeneratorAction.hh
+/// \brief Definition of the B1PrimaryGeneratorAction class
 
-#ifndef B1EventAction_h
-#define B1EventAction_h 1
+#ifndef B1PrimaryGeneratorAction_h
+#define B1PrimaryGeneratorAction_h 1
 
-#include "G4UserEventAction.hh"
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
 #include "globals.hh"
 
-/// Event action class
+class G4ParticleGun;
+class G4Event;
+class G4Box;
+
+/// The primary generator action class with particle gun.
 ///
+/// The default kinematic is a 6 MeV gamma, randomly distribued 
+/// in front of the phantom across 80% of the (X,Y) phantom size.
 
-namespace B1
-{
-
-class RunAction;
-
-class EventAction : public G4UserEventAction
+class B1PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
   public:
-    EventAction(RunAction* runAction);
-    ~EventAction() override = default;
+    B1PrimaryGeneratorAction();    
+    virtual ~B1PrimaryGeneratorAction();
 
-    void BeginOfEventAction(const G4Event* event) override;
-    void EndOfEventAction(const G4Event* event) override;
-
-    void AddEdep(G4double edep) { fEdep += edep; }
-
+    // method from the base class
+    virtual void GeneratePrimaries(G4Event*);         
+  
+    // method to access particle gun
+    const G4ParticleGun* GetParticleGun() const { return fParticleGun; }
+  
   private:
-    RunAction* fRunAction = nullptr;
-    G4double   fEdep = 0.;
+    G4ParticleGun*  fParticleGun; // pointer a to G4 gun class
+    G4Box* fEnvelopeBox;
 };
-
-}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
