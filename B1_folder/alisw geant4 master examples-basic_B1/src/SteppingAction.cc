e46db9dcdad2e0046d43a1248bc85d3c396c28fb
abbyxh <157227938+abbyxh@users.noreply.github.com>
2024-03-20 14:12:30 +0000

    Update SteppingAction.cc
    fScoringVolume to a list

--- B1_folder/alisw geant4 master examples-basic_B1/src/SteppingAction.cc
+++ B1_folder/alisw geant4 master examples-basic_B1/src/SteppingAction.cc
@@ -24,50 +24,59 @@
 // ********************************************************************
 //
 //
-/// \file B1/src/SteppingAction.cc
-/// \brief Implementation of the B1::SteppingAction class
+/// \file B1SteppingAction.cc
+/// \brief Implementation of the B1SteppingAction class
 
-#include "SteppingAction.hh"
-#include "EventAction.hh"
-#include "DetectorConstruction.hh"
+#include <vector>
+#include <iostream>
+
+#include "B1SteppingAction.hh"
+#include "B1EventAction.hh"
+#include "B1DetectorConstruction.hh"
 
 #include "G4Step.hh"
 #include "G4Event.hh"
 #include "G4RunManager.hh"
 #include "G4LogicalVolume.hh"
 
-namespace B1
-{
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-SteppingAction::SteppingAction(EventAction* eventAction)
-: fEventAction(eventAction)
+B1SteppingAction::B1SteppingAction(B1EventAction* eventAction)
+: G4UserSteppingAction(),
+  fEventAction(eventAction)
 {}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-void SteppingAction::UserSteppingAction(const G4Step* step)
-{
-  if (!fScoringVolume) {
-    const auto detConstruction = static_cast<const DetectorConstruction*>(
-      G4RunManager::GetRunManager()->GetUserDetectorConstruction());
-    fScoringVolume = detConstruction->GetScoringVolume();
-  }
-
-  // get volume of the current step
-  G4LogicalVolume* volume
-    = step->GetPreStepPoint()->GetTouchableHandle()
-      ->GetVolume()->GetLogicalVolume();
-
-  // check if we are in scoring volume
-  if (volume != fScoringVolume) return;
-
-  // collect energy deposited in this step
-  G4double edepStep = step->GetTotalEnergyDeposit();
-  fEventAction->AddEdep(edepStep);
-}
+B1SteppingAction::~B1SteppingAction()
+{}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
+void B1SteppingAction::UserSteppingAction(const G4Step* step)
+{
+  if (fScoringVolume.empty()) { 
+    const B1DetectorConstruction* detectorConstruction
+      = static_cast<const B1DetectorConstruction*>
+        (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
+    fScoringVolume = detectorConstruction->GetScoringVolume();   
+  }
+
+  // get volume of the current step
+  G4LogicalVolume* volume 
+    = step->GetPreStepPoint()->GetTouchableHandle()
+      ->GetVolume()->GetLogicalVolume();
+      
+  // check if we are in scoring volume using a look to go through the list of scoring volumes
+  int check{0};
+  for (G4LogicalVolume * vol: fScoringVolume) {
+    if (volume == vol) ++check;
+  }
+  if (check == 0) return;
+
+  // collect energy deposited in this step
+  G4double edepStep = step->GetTotalEnergyDeposit();
+  fEventAction->AddEdep(edepStep);  
 }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
